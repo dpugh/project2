bceabce57d9272bd26e123307f00d5568646cb04
David Pugh <dpugh@microsoft.com>
2023-08-01 21:20:48 -0700

    checkpoint

--- main.py
+++ main.py
@@ -1,51 +1,87 @@
+"""
+This animation example shows how perform a radar sweep animation.
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.radar_sweep
+"""
+
 import arcade
+import math
 
-# Set constants for the screen size
-SCREEN_WIDTH = 600
+# Set up the constants
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Radar Sweep Example"
 
-# Open the window. Set the window title and dimensions (width and height)
-arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing Example")
+# These constants control the particulars about the radar
+CENTER_X = SCREEN_WIDTH // 2
+CENTER_Y = SCREEN_HEIGHT // 2
+RADIANS_PER_FRAME = 0.02
+SWEEP_LENGTH = 250
 
-# Set the background color to white.
-# For a list of named colors see:
-# http://arcade.academy/arcade.color.html
-# Colors can also be specified in (red, green, blue) format and
-# (red, green, blue, alpha) format.
-arcade.set_background_color(arcade.color.WHITE)
 
-# Start the render process. This must be done before any drawing commands.
-arcade.start_render()
+class Radar:
+    def __init__(self):
+        self.angle = 0
 
-# Draw the face
-x = 300
-y = 300
-radius = 200
-arcade.draw_circle_filled(x, y, radius, arcade.color.YELLOW)
+    def update(self):
 
-# Draw the right eye
-x = 370
-y = 350
-radius = 20
-arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
+        # Move the angle of the sweep.
+        self.angle += RADIANS_PER_FRAME
 
-# Draw the left eye
-x = 230
-y = 350
-radius = 20
-arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
+    def draw(self):
+        """ Use this function to draw everything to the screen. """
 
-# Draw the smile
-x = 300
-y = 280
-width = 120
-height = 100
-start_angle = 190
-end_angle = 350
-arcade.draw_arc_outline(x, y, width, height, arcade.color.BLACK, start_angle, end_angle, 10)
+        # Calculate the end point of our radar sweep. Using math.
+        x = SWEEP_LENGTH * math.sin(self.angle) + CENTER_X
+        y = SWEEP_LENGTH * math.cos(self.angle) + CENTER_Y
 
-# Finish drawing and display the result
-arcade.finish_render()
+        # Start the render. This must happen before any drawing
+        # commands. We do NOT need an stop render command.
+        arcade.start_render()
 
-# Keep the window open until the user hits the 'close' button
-arcade.run()
+        # Draw the radar line
+        arcade.draw_line(CENTER_X, CENTER_Y, x, y, arcade.color.OLIVE, 4)
+
+        # Draw the outline of the radar
+        arcade.draw_circle_outline(CENTER_X,
+                                   CENTER_Y,
+                                   SWEEP_LENGTH,
+                                   arcade.color.DARK_GREEN,
+                                   border_width=10,
+                                   num_segments=60)
+
+
+class MyGame(arcade.Window):
+    """ Main application class. """
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        # Create our rectangle
+        self.radar = Radar()
+
+        # Set background color
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on_update(self, delta_time):
+        # Move the rectangle
+        self.radar.update()
+
+    def on_draw(self):
+        """ Render the screen. """
+
+        # Clear screen
+        self.clear()
+        # Draw the rectangle
+        self.radar.draw()
+
+
+def main():
+    """ Main function """
+    MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
